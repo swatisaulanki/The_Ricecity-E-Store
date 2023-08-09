a83d3ed1620d933bf3f70f8f1d52e1eabd6b8c41
PoojaD12345 <dhuripooja01@gmail.com>
2023-08-09 22:13:03 +0530

    feature product page updated

--- client/src/Pages/Cart/Cartpage.jsx
+++ client/src/Pages/Cart/Cartpage.jsx
@@ -1,66 +1,81 @@
-import axios from "axios";
+import {
+  Box,
+  Button,
+  Grid,
+  GridItem,
+  HStack,
+  Heading,
+  Text,
+} from "@chakra-ui/react";
 import React, { useEffect, useState } from "react";
-import { useParams } from "react-router-dom";
-import styles from "./Cartpage.module.css";
+import axios from "axios";
+import { Link } from "react-router-dom";
 const Cartpage = () => {
-  const [data, setData] = useState([]);
-  const [cart, setCart] = useState([]);
-  const { id } = useParams();
+  const [products, setProducts] = useState([]);
 
-  console.log(data);
-  const GetData = () => {
-    axios.get("http://localhost:3000/CartsDatas").then((res) => {
+  const getAllData = async () => {
+    try {
+      const res = await axios.get("http://localhost:8000/CartsDatas");
       console.log(res.data);
-      setData(res.data);
-    });
-  };
-  useEffect(() => {
-    GetData();
-  }, []);
-
-  const handleDelete = () => {
-    console.log(data.id);
-    alert("data");
-    axios
-      .delete(`http://localhost:3000/CartsDatas{id}`)
-      .then(() => {
-        alert("data deleted succesfully");
-        GetData();
-      })
-      .catch((err) => {
-        console.log(err);
-      });
+      setProducts(res.data);
+    } catch (err) {
+      console.log(err);
+    }
   };
 
   useEffect(() => {
-    GetData();
+    getAllData();
   }, []);
 
   return (
-    <div style={{display:"grid" ,gridTemplateColumns: "repeat(4,1fr)", gap:"20px"}}>
-      {data.length > 0 &&
-        data.map((item) => {
-          return (
-            <div className={styles.Cartcontainstwo}>
-              <div className={styles.imgcont}>
-                <img src={item.imageURL2} />
+    <>
+      <Box py={"10px"}>
+        <Heading py={"20px"}>ALL PRODUCTS</Heading>
+        <Grid
+          templateColumns={[
+            "repeat(1, 1fr)",
+            "repeat(1, 1fr)",
+            "repeat(2, 1fr)",
+            "repeat(4, 1fr)",
+          ]}
+          gap={6}
+          w={"90%"}
+          m={"auto"}
 
-                <h1>{item.title}</h1>
-              </div>
-
-              <div className={styles.Cartname}>
-                <p>{item.name}</p>
-
-                <p>{item.price}</p>
-              </div>
-
-              <div className={styles.btncart}>
-                <button> Add to Cart </button>
-              </div>
-            </div>
-          );
-        })}
-    </div>
+          // border={"1px solid red"}
+        >
+          {products.map((e) => (
+            <GridItem height={"300px"} boxShadow="rgba(0, 0, 0, 0.16) 0px 1px 4px">
+              {/* <Link to={`/singlesale/${e.id}`}> */}
+                <Box
+                  key={e.id}
+                  _hover={{ backgroundSize: "120%" }}
+                  position="relative"
+                  backgroundPosition="center"
+                  backgroundRepeat="no-repeat"
+                  backgroundSize="cover"
+                  overflow={"hidden"}
+                  height={"200px"}
+                  cursor={"pointer"}
+                  border={"1px solid grey"}
+                  m={"10px"}
+                  borderRadius={"5px"}
+                  backgroundImage={`url(${e.imageURL2})`}
+                  transition="background-size 0.5s ease"
+                >
+                  <Box backgroundColor={"#a4e1f4"} fontWeight={500} color={"#E22D4A"} w={"50%"} mt={"175px"}>₹{e.pricecashback}</Box>
+                </Box>
+                
+              {/* </Link> */}
+              <Box px={"5px"}>
+                <Text noOfLines={1}>{e.name}</Text>
+                <HStack m={"auto"} justifyContent={"space-around"} color={"#E22D4A"}><Text fontWeight={500}><span style={{color:"#E22D4A"}}>₹</span> {e.price}</Text><Button _hover={{ backgroundColor: "#bbd9c1" }} backgroundColor={"#bbd9c1 "} color={"white"} >Add To Cart</Button></HStack>
+              </Box>
+            </GridItem>
+          ))}
+        </Grid>
+      </Box>
+    </>
   );
 };
 
