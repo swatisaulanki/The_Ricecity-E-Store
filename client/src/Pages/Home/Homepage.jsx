3f7f6b806d9025bd143f95e45fc3caf53752bd43
PoojaD12345 <dhuripooja01@gmail.com>
2023-08-12 14:44:01 +0530

    homepage updated

--- client/src/Pages/Home/Homepage.jsx
+++ client/src/Pages/Home/Homepage.jsx
@@ -1,9 +1,9 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Slider from "react-slick";
-import { IconButton, useBreakpointValue, Image, Box, Button } from "@chakra-ui/react";
+import { IconButton, useBreakpointValue, Image, Box, Button, Heading, Grid, GridItem, Text, HStack } from "@chakra-ui/react";
 import { BiLeftArrowAlt, BiRightArrowAlt } from "react-icons/bi";
-import Cartpage from "../Cart/Cartpage";
 import { Link } from "react-router-dom";
+import axios from "axios";
 
 // Settings for the sliderr
 const settings = {
@@ -19,6 +19,21 @@ const settings = {
 };
 
 const Homepage = () => {
+  const [products, setProducts] = useState([]);
+
+  const getAllData = async () => {
+    try {
+      const res = await axios.get("http://localhost:8000/CartsDatas");
+      console.log(res.data);
+      setProducts(res.data);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  useEffect(() => {
+    getAllData();
+  }, []);
   const [slider, setSlider] = React.useState(0);
 
   const top = useBreakpointValue({ base: "90%", md: "50%" });
@@ -105,7 +120,52 @@ const Homepage = () => {
         </Slider>
       </Box>
 
-      <Cartpage/>
+      <Box py={"10px"}>
+        <Heading py={"20px"}>ALL PRODUCTS</Heading>
+        <Grid
+          templateColumns={[
+            "repeat(1, 1fr)",
+            "repeat(1, 1fr)",
+            "repeat(2, 1fr)",
+            "repeat(4, 1fr)",
+          ]}
+          gap={6}
+          w={"90%"}
+          m={"auto"}
+
+          // border={"1px solid red"}
+        >
+          {products.map((e) => (
+            <GridItem height={"300px"} boxShadow="rgba(0, 0, 0, 0.16) 0px 1px 4px">
+              {/* <Link to={`/singlesale/${e.id}`}> */}
+                <Box
+                  key={e.id}
+                  _hover={{ backgroundSize: "120%" }}
+                  position="relative"
+                  backgroundPosition="center"
+                  backgroundRepeat="no-repeat"
+                  backgroundSize="cover"
+                  overflow={"hidden"}
+                  height={"200px"}
+                  cursor={"pointer"}
+                  border={"1px solid grey"}
+                  m={"10px"}
+                  borderRadius={"5px"}
+                  backgroundImage={`url(${e.imageURL2})`}
+                  transition="background-size 0.5s ease"
+                >
+                  <Box backgroundColor={"#a4e1f4"} fontWeight={500} color={"#E22D4A"} w={"50%"} mt={"175px"}>₹{e.pricecashback}</Box>
+                </Box>
+                
+              {/* </Link> */}
+              <Box px={"5px"}>
+                <Text noOfLines={1}>{e.name}</Text>
+                <HStack m={"auto"} justifyContent={"space-around"} color={"#E22D4A"}><Text fontWeight={500}><span style={{color:"#E22D4A"}}>₹</span> {e.price}</Text><Button _hover={{ backgroundColor: "#bbd9c1" }} backgroundColor={"#bbd9c1 "} >Add To Cart</Button></HStack>
+              </Box>
+            </GridItem>
+          ))}
+        </Grid>
+      </Box>
 
       <Link to={"/store"}><Button backgroundColor={"#5cb85c"} borderRadius={"0px"} fontWeight={600} fontSize={"20px"} p={"30px"}>Load More</Button></Link>
       {/* <SimpleGrid
